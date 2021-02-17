1b1e940a90ee1fe99171fa4baf723a64c83dcf09
BlueSmiley <BlueSmiley@users.noreply.github.com>
2021-02-17 20:36:21 +0000

    Basic required functionality added, now to brush up and clean up results

--- src/VoucherCreationManager.js
+++ src/VoucherCreationManager.js
@@ -11,6 +11,35 @@ class VoucherCreationManager extends React.Component {
     handleCreateVoucher(event, businessId, clientId, creatingBranchId, expiryDate, issueDate, originalBalance) 
     {
         event.preventDefault();
+        const url = 'http://api-gateway-dev.phorest.com/third-party-api-server/api/business/' + businessId + '/voucher';
+        const requestBody = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + btoa('global/cloud@apiexamples.com:VMlRo/eh+Xd8M~l')
+            },
+            body: JSON.stringify({ 
+                clientId: clientId,
+                creatingBranchId: creatingBranchId,
+                expiryDate: expiryDate,
+                issueDate: issueDate,
+                originalBalance: originalBalance
+            })
+        };
+        fetch(url, requestBody)
+            .then((res) => {
+                // better status checking later maybe
+                if (!res.ok) {
+                    throw new Error(res.status);
+                }
+                return res.json();
+            })
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.log('error:' + error);
+            })
         console.log("Business Id:" + businessId + " Client Id:" + clientId + " Branch Id:" + creatingBranchId +
             " Expiry date:" + expiryDate + " Issue Date:" + issueDate + " Original Balance:" + originalBalance)
     }
